1416dd9ccc833f9c0eac77881cfe0eb3db38961b
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2021-06-27 13:54:07 +0200

    Raise coverage to 100% Add coverage report in ci pipeline

--- rusty_results/tests/option/test_option_some.py
+++ rusty_results/tests/option/test_option_some.py
@@ -118,12 +118,6 @@ def test_some_zip_with():
     assert some.zip_with(Some(5), lambda x: sum(x)) == Some(value + 5)
 
 
-def test_some_expect_none():
-    some, value = create_some()
-    with pytest.raises(Exception):
-        some.expect_empty("")
-
-
 def test_some_unwrap_empty():
     some, value = create_some()
     with pytest.raises(Exception):
