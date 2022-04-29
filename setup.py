1d3f6428ae8d94ef73e89ecd73c14e1775e9ca3e
check69 <antonio.checa.bustos@gmail.com>
2022-04-29 21:03:23 +0100

    Add typed for mypy as third party library

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
+    package_data={".": ["py.typed"]},
     packages=setuptools.find_packages(
         ".",
         exclude=[
