217e7d0ecb1ef5290636d9027e69d8ae3481e964
Alex Cabeza <alex93cabeza@gmail.com>
2022-05-14 12:25:15 +0200

    Exclude tests from packaging. (#13)

--- setup.py
+++ setup.py
@@ -11,21 +11,15 @@ setuptools.setup(
     description="Rust inspired Result and Option types",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/danielSanchezQ/rusty_results",
     package_data={".": ["py.typed"]},
-    packages=setuptools.find_packages(
-        ".",
-        exclude=[
-            "tests*",
-            "examples*"
-        ]
-    ),
+    packages=setuptools.find_packages(exclude=("*tests*",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     extras_require={
-        'pydantic': ["pydantic"]
+        "pydantic": ["pydantic"]
     },
     python_requires='>=3.7'
 )
