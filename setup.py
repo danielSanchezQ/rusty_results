0679497c789fba9d515d8ad939b2ec81f87a8887
Antonio José Checa Bustos <antonio.checa.bustos@gmail.com>
2022-05-16 08:38:57 -0500

    Fix mypy by adding py.typed to the package build (#17)

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rusty-results",
-    version="1.1.0",
+    version="1.1.1",
     author="Daniel Sanchez Quiros, Antonio Jose Checa Bustos",
     author_email="sanchez.quiros.daniel@gmail.com, antonio.checa.bustos@gmail.com",
     description="Rust inspired Result and Option types",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/danielSanchezQ/rusty_results",
-    package_data={".": ["py.typed"]},
+    package_data={"rusty_results": ["py.typed"]},
     packages=setuptools.find_packages(exclude=("*tests*",)),
     classifiers=[
         "Programming Language :: Python :: 3",
